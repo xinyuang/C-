c9091642b2d5f9d1b3842188c00f233b4f1ee043
xinyuang <xinyuang@umich.edu>
2018-05-25 23:15:31 -0400

    10

--- DP/Sell_Stock/188. Best Time to Buy and Sell Stock IV.cpp
+++ DP/Sell_Stock/188. Best Time to Buy and Sell Stock IV.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
@@ -23,4 +24,31 @@ public:
         return se_v[k];
         }
     }
+=======
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        if (prices.empty() || k == 0) return 0;
+        if(k >= prices.size()/2) {
+            int se = 0, sh = INT_MIN;
+            for (int price : prices) {
+                se = max(se, sh + price);
+                sh = max(sh, se - price);
+            }
+            return se;
+        }
+        else {
+            vector<int> se_v(k + 1, 0);
+            vector<int> sh_v(k + 1, INT_MIN);
+            for (int price : prices) {
+                for(int i = k; i > 0; i--) {
+                    
+                    se_v[i] = max(se_v[i], sh_v[i] + price);
+                    sh_v[i] = max(sh_v[i], se_v[i - 1] - price);
+                }
+            }
+        return se_v[k];
+        }
+    }
+>>>>>>> 904c0daf6776a91c1087079e8582f026fb3b8022
 };
--- DP/Sell_Stock/309. Best Time to Buy and Sell Stock with Cooldown.cpp
+++ DP/Sell_Stock/309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -28,4 +29,36 @@ public:
         }
         return max(sc[n - 1], se[n - 1]);
     }
+=======
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int se = 0, sh = INT_MIN, se_old = 0, se_pre = 0;
+        for (int price : prices) {
+            se_old = se;
+            se = max(se, sh + price);
+            sh = max(sh, se_pre - price);
+            se_pre = se_old;
+        }
+        return se;
+    }
+};
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n == 0) return 0;
+        vector<int> se(n), sh(n), sc(n);
+        se[0] = 0;
+        sh[0] = -prices[0];
+        sc[0] = INT_MIN;
+        for (int i = 1; i < n; i++) {
+            se[i] = max(se[i - 1], sc[i - 1]);
+            sh[i] = max(sh[i - 1], se[i - 1] - prices[i]);
+            sc[i] = sh[i - 1] + prices[i];
+        }
+        return max(sc[n - 1], se[n - 1]);
+    }
+>>>>>>> 904c0daf6776a91c1087079e8582f026fb3b8022
 };
